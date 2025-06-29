07c0e090c0d68529136296bf181992595cba3f85
tadkjohnson <tadkjohnson@gmail.com>
2025-06-29 15:05:03 -0500

    py w1 assign1

---
+++ idbadge.py
@@ -0,0 +1,11 @@
+# needs first, last, email, phone, job title, and an id number  
+# show info in order of  1st line last, first   2nd line job title   3rd line  id   space   line email and line phone number
+
+lastname = input ("What is your last name? ")
+firstname = input ("What is your first name? ")
+email = input ("What is your email address? ")
+title = input ("What is your job title name? ")
+idn = input ("What is your ID Number name? ")
+
+
+
