c9b91aab29133bb2fec658dd0833a0822a256c20
tadkjohnson <tadkjohnson@gmail.com>
2025-06-29 16:24:35 -0500

    week1

---
+++ random info/titles.py
@@ -0,0 +1,10 @@
+# all the different ways to cap/lower case things.
+
+tested = "This is the INFORmation"
+print(tested)
+print(f"{tested.title()}")
+print(f"{tested.capitalize()}")
+print(f"{tested.upper()}")
+print(f"{tested.lower()}")
+print(f"{tested.count(i)}")
+
