15dd9d0141166346b1a882cd6cc0de77d6337dd7
tadkjohnson <tadkjohnson@gmail.com>
2025-07-04 15:00:25 -0500

    area

--- week2/area.py
+++ week2/area.py
@@ -6,6 +6,7 @@ print("Get the area of a shape")
 sq_side = float(input("what is a sides length of the square? "))
 sq_area = sq_side * sq_side
 print(f"the area of the square is: {sq_area:.3f}")
+# print(f"the area of the square is: {sq_side **2:.3f}") another way to do it 
 print()
 print("Get the area of a circle")
 circle = float(input("what is a radius of the circle? "))
@@ -17,7 +18,7 @@ rec_length = float(input("what is the length of the rectangle? "))
 rec_width = float(input("what is the width of the rectangle? "))
 rec_area = rec_length * rec_width
 print(f"the area of the rectangle is: {rec_area:.3f}")
-# print(f"the area of the rectangle is {rec_length * rec_width}")
+# print(f"the area of the rectangle is {rec_length * rec_width}") another way to do it 
 
 
 
