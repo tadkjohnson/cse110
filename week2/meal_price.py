cb1652f8deced87684ebaca92c778cc22c2f747b
tadkjohnson <tadkjohnson@gmail.com>
2025-07-04 18:23:47 -0500

    sync

---
+++ week2/meal_price.py
@@ -0,0 +1,48 @@
+import math
+
+
+#added senior citizens cost, several tip suggestions, and drinks.
+cost_child = float(input("what is the price of the child meal? $ "))
+cost_adult = float(input("what is the price of the adult meal? $ "))
+cost_senior = float(cost_adult - (cost_adult * .25))
+
+num_child = int(input("how many child meals were purchased? "))
+num_adult = int(input("how many adult meals were purchased? "))
+num_senior = int(input("how many senior meals were purchased? "))
+
+tally_child = cost_child * num_child
+tally_adult = cost_adult * num_adult 
+tally_senior = cost_senior * num_senior
+
+drink_child = int(input("how many child drinks were purchased? "))
+drink_adult = int(input("how many adult drinks were purchased? "))
+drink_senior = int(input("how many senior drinks were purchased? "))
+
+drink_cost_c = 1
+drink_cost_a = 2.50
+drink_cost_s = 1.50
+
+drinks_total = float((drink_child * drink_cost_c) + (drink_cost_a * drink_adult) + (drink_cost_s * drink_senior))
+
+subtotal = float(tally_child + tally_adult + tally_senior + drinks_total)
+tax_rate = float(input("what is the tax rate (entered as a percentage)"))
+print()
+print()
+tax_amount = float(math.ceil(subtotal * tax_rate)/100)
+print(f"The total before Tax and Tip is {subtotal:.2f}")
+print(f"Tax charge :{tax_amount:.6f}")
+print()
+print(f"Total bill with taxes ${subtotal + tax_amount:.2f}")
+print()
+print(f"suggested tip 15% {subtotal * .15:.2f}, which would make your total {subtotal + (subtotal * .15):.2f}")
+print(f"suggested tip 20% {subtotal * .2:.2f}, which would make your total {subtotal + (subtotal * .2):.2f}")
+print(f"suggested tip 25% {subtotal * .25:.2f}, which would make your total {subtotal + (subtotal * .25):.2f}")
+print()
+print()
+customers_tip = float(input("How much do you want to tip?" ))
+
+print(f"Your total bill with tip was {customers_tip + subtotal}:.2f")
+customers_payment = float(input("how much did you pay $"))
+print(f"Your change is {customers_payment - (subtotal + customers_tip)}:.2f")
+
+
