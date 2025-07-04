4389ae14dc4e0978f71eef197b9d453c10ed8af9
tadkjohnson <tadkjohnson@gmail.com>
2025-07-04 14:53:16 -0500

    shapes

---
+++ random info/area_shapes.py
@@ -0,0 +1,30 @@
+ 
+print("Get the area of a shape")
+ar_sh = input("is your shape a circle, a square, or a rectangle? c/s/r: ").lower()
+
+let_to_num = {
+c : 0,
+s : 5,
+r : 10,
+}
+
+if ar_sh = c
+    radius = float(input ("what is the radius? "))
+    print(f{radius * 3.14} "is the area of a circle")
+elif ar_sh = 5
+    sq_1 = float(input ("what is the length of a side? "))
+    print(f{sq_1*sq_1}"is the area of the square")
+else ar_sh = 10
+    rec_1 = float(input ("what is the length of a side? "))
+    rec_2 = float(input ("what is the length of the second side? "))
+    print(f{rec_1 * rec_2}"the area of the rectangle")
+
+
+
+# square
+# print("is your shape a square?")
+
+# rectangle
+# print("is your shape a rectangle?")
+# triangle
+# print("is your shape a triangle?")
---
+++ week2/area.py
@@ -0,0 +1,24 @@
+    # radius = float(input ("what is the radius? "))
+
+
+ 
+print("Get the area of a shape")
+sq_side = float(input("what is a sides length of the square? "))
+sq_area = sq_side * sq_side
+print(f"the area of the square is: {sq_area:.3f}")
+print()
+print("Get the area of a circle")
+circle = float(input("what is a radius of the circle? "))
+cir_area = circle * 3.14
+print(f"the area of the circle is: {cir_area:.3f}")
+print()
+print("Get the area of a rectangle")
+rec_side1 = float(input("what is the first sides length of the rectangle? "))
+rec_side2 = float(input("what is the second sides length of the rectangle? "))
+rec_area = rec_side1 * rec_side2
+print(f"the area of the rectangle is: {rec_area:.3f}")
+
+
+
+
+
