88b447942e264a31e45f65be8dc90acc9ebef100
tadkjohnson <tadkjohnson@gmail.com>
2025-07-04 13:45:46 -0500

    start placeholder and fixed

--- cleverstory.py
+++ cleverstory.py
@@ -11,9 +11,9 @@ friend_name = input("name one of your friends: ")
 exclamation = input ("What is something to exclaim? ")
 adjective = input ("Any adjective: ")
 animal = input ("Favorite animal: ")
-verb = input ("Any verb: ")
-verb2 = input ("Any other verb: ")
-verb3 = input ("And a different verb: ")
+verb_1 = input ("Any verb: ")
+verb_2 = input ("Any other verb: ")
+verb_3 = input ("And a different verb: ")
 activity = input ("what is your hobby? ")
 
 print("\n")
@@ -26,9 +26,9 @@ story = f"""When I, {first_name.capitalize()} {last_name.capitalize()}, returned
 print(story)
 print("\n")
 story = f"""The other day, I was really in trouble. It all started when I saw a very
-    {adjective.lower()} {animal.lower()} {verb} down the hallway. {exclamation.title()}! I yelled. But all
-    I could think to do was to {verb2} over and over. Miraculously,
-    that caused it to stop, but not before it tried to {verb3} 
+    {adjective.lower()} {animal.lower()} {verb_1} down the hallway. {exclamation.title()}! I yelled. But all
+    I could think to do was to {verb_2} over and over. Miraculously,
+    that caused it to stop, but not before it tried to {verb_3} 
     right in front of my family."""
 print(story)
 print("-" * 50)
--- demo.py
+++ demo.py
@@ -1,1 +1,11 @@
 print ("Hello you Awesome People!")
+
+
+
+first = input("First name: ")
+print(f"{first.upper()}")
+print()
+print(f"{first.title()}")
+print()
+print()
+print(first)
